ee299315fc4e6afbb0422fec4c676ef446e20b19
vrodnenok <victor.r@ukr.net>
2022-04-13 18:29:31 +0300

    changed table

--- emailer/models.py
+++ emailer/models.py
@@ -8,17 +8,8 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=75)
     last_name = models.CharField(max_length=75)
     company_name = models.CharField(max_length=75)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     is_broker = models.BooleanField(default=True)
     is_owner = models.BooleanField(default=True)
     is_charterer = models.BooleanField(default=True)
     comment = models.TextField()
-
-
-class ContactForm(ModelForm):
-    class Meta:
-        model = Contact
-        fields = [
-            # , 'company', 'is_broker', 'is_owner', 'is_charterer', 'comment'
-            'first_name', 'last_name', 'company_name', 'email'
-        ]
